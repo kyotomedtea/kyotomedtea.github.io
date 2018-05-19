94d26d5d4211e4e1d9133972914294083fda8c80
meow2 <kyotomedtea@mao-yoshimura.com>
2018-05-20 08:23:09 +0900

    added comment

--- js/database.js
+++ js/database.js
@@ -1,8 +1,11 @@
 var originalJSON;
-$(document).ready(function(){  //読み込み時
+
+$(document).ready(function(){  //読み込み時に動作
   //var jsonURL = "https://script.google.com/macros/s/AKfycbwzEIf805yqL4ptzS9tdY8Xd5_I6rTV0dH-HebDY2-SFL1KkBR-/exec"; //古い方 test用
   var jsonURL = "https://script.google.com/macros/s/AKfycbx-dueD6pv4UmFP82zYsE803Nzh4MYY-Uaqa7B7HLhFK-cAig/exec"; //新しい方
-  $.ajax({
+  //上記jsonURLはGoogleドライブ上にある、doGET関数を持つserver.gsのdeploy as web appのURL
+
+  $.ajax({ //ajaxのGET方式でJSONデータを取りに行く
     type: 'GET',
     url: jsonURL,
     dataType: 'json',
@@ -17,16 +20,18 @@ $(document).ready(function(){  //読み込み時
   }); //$.ajax終了
 }); //$(document).readyの終了
 
-function JSON2HTML(json){
+function JSON2HTML(json){ //JSONをHTMLのtableに整形する
   var HTMLtable = "<table id = \"db_table\"><th>カテゴリ</th><th>名称</th><th>画像</th><th>説明</th>";
   for (i = 0; i < json.length; i++) {
     var id = "ID" +  i;
     HTMLtable += "<tr><td>" + json[i]["category"] + "</td>";
     HTMLtable += "<td><a onclick=\"OnLinkClick(" + i + ");\">" + json[i]["name"] + "</a></td>";
-    HTMLtable += "<td><a href = \"" + json[i]["image1"] + "\" class = \"itemimg\" rel = \"" + json[i]["name"] + i + "\" title = \"" + json[i]["name"] + "\" id = \"" + id + "\"><img src = \"" + json[i]["image1"] + "\"></a>"; //</td閉じてない>
-    if(json[i]["image2"]){
+    //名前をクリックすると、画像をクリックしたのと同じ動作をする（colorboxで表示する画像がダブらないように）
+    HTMLtable += "<td><a href = \"" + json[i]["image1"] + "\" class = \"itemimg\" rel = \"" + json[i]["name"] + i + "\" title = \"" + json[i]["name"] + "\" id = \"" + id + "\"><img src = \"" + json[i]["image1"] + "\"></a>";
+    //</td>閉じてないの注意。class="itemimg"に対してcolorboxが動作。rel=同じIDに対して同じグループとしてcolorboxが動作。
+    if(json[i]["image2"]){ //2個目の画像
       HTMLtable += "<div style=\"display: none;\"><a href = \"" + json[i]["image2"] + "\" class = \"itemimg\" rel = \"" + json[i]["name"] +　i + "\" title = \"" + json[i]["name"] + "\"><img src = \"" + json[i]["image2"] + "\"></a>";
-      if(json[i]["image3"]){
+      if(json[i]["image3"]){ //3個目の画像
         HTMLtable += "<a href = \"" + json[i]["image3"] + "\" class = \"itemimg\" rel = \"" + json[i]["name"] + i + "\" title = \"" + json[i]["name"] + "\"><img src = \"" + json[i]["image3"] + "\"></a>";
       }
       HTMLtable += "</div>";
@@ -35,15 +40,16 @@ function JSON2HTML(json){
     HTMLtable += "<td>" + json[i]["description"] + "</td></tr>";
   } //forループ終了
   HTMLtable += "</table>";
-  $(function(){
+
+  $(function(){ //colorboxはここで動作させる。ページ読み込み時点ではHTML tableはできていないので。
     $(".itemimg").colorbox();
   });
-  //console.log(HTMLtable);
+
   document.getElementById("numberOfResults").innerHTML = "<p>該当データ数：" + json.length + "件</p>";
   document.getElementById("DBcontents").innerHTML = HTMLtable;
 }
 
-function modifyJSON(json, kind, search){
+function modifyJSON(json, kind, search){ //絞込や検索に応じてJSONをいじる
   var modifiedJSON　= [];
   for (i = 0; i < json.length; i++) {
     if ((json[i]["category"] == kind || kind == "全て") && (json[i]["name"].indexOf(search) != -1 || json[i]["description"].indexOf(search) != -1)) {
@@ -56,12 +62,12 @@ function modifyJSON(json, kind, search){
   }
 }
 
-function OnLinkClick(id_no){
+function OnLinkClick(id_no){ //HTML tableで、名称をクリックすると画像をクリックしたのと同じ動作をさせる
   var id = "ID" + id_no;
   document.getElementById(id).click();
 }
 
-$(function(){
+$(function(){ //エンターキーを押しても動作するようにする
   $("input[type=text]").keypress(function(ev) {
     if ((ev.which && ev.which === 13) ||
     (ev.keyCode && ev.keyCode === 13)) {
@@ -70,8 +76,4 @@ $(function(){
       return true;
     }
   });
-});
-
-$(function(){
-  $(".itemimg").colorbox();
 });

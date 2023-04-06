867a803e37275f9b7dd8eebcb6574255d660070e
kyotomedtea <kyoto.medical.tea@gmail.com>
2023-04-06 19:26:09 +0900

    Update jsmail.js

--- js/jsmail.js
+++ js/jsmail.js
@@ -44,3 +44,11 @@ function secretaddress_2022(){
 	var mt = String.fromCharCode(109,97,105,108,116,111,58);
 	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');	
 }
+
+//2023年度新歓用
+function secretaddress_2023_twitter(){
+	var ma = String.fromCharCode(107,121,111,116,111,109,101,100,116,101,97,50,48,50,50,115,104,105,110,107,97,110,64,103,109,97,105,108,46,99,111,109);
+	var mt = String.fromCharCode(109,97,105,108,116,111,58);
+	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');	
+}
+

06b368c2168ed0710af6b32f3bbd83f00b82b5e7
Naoto Ohashi <onaoto2014@gmail.com>
2026-02-25 23:27:54 +0900

    閉鎖メッセージ掲載。

--- js/jsmail.js
+++ js/jsmail.js
@@ -1,9 +1,43 @@
 function secretaddress() {
-	var ma = String.fromCharCode(112-5,107+14,111-0,108+8,142-31,24+22,133-24,95+6,117-17,111+5,129-28,74+23,60-10,28+20,72-23,27+30,91-27,79+24,109-0,67+30,122-17,106+2,58-12,78+21,113-2,104+5);
-	var mt = String.fromCharCode(147-38,69+28,146-41,52+56,143-27,61+50,105-47);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+  var ma = String.fromCharCode(
+    112 - 5,
+    107 + 14,
+    111 - 0,
+    108 + 8,
+    142 - 31,
+    24 + 22,
+    133 - 24,
+    95 + 6,
+    117 - 17,
+    111 + 5,
+    129 - 28,
+    74 + 23,
+    60 - 10,
+    28 + 20,
+    72 - 23,
+    27 + 30,
+    91 - 27,
+    79 + 24,
+    109 - 0,
+    67 + 30,
+    122 - 17,
+    106 + 2,
+    58 - 12,
+    78 + 21,
+    113 - 2,
+    104 + 5
+  );
+  var mt = String.fromCharCode(
+    147 - 38,
+    69 + 28,
+    146 - 41,
+    52 + 56,
+    143 - 27,
+    61 + 50,
+    105 - 47
+  );
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
-
 
 // http://www.nishishi.com/scripts/jsmail/
 //件名→行きたい新歓
@@ -19,36 +53,182 @@ function secretaddress() {
 //http://www.tagindex.com/tool/url.html
 //ここで変換
 
-
 function secretaddress2() {
-	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,116,101,97,51,48,64,103,109,97,105,108,46,99,111,109);
-	var mt = String.fromCharCode(109,97,105,108,116,111,58);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+  var ma = String.fromCharCode(
+    107,
+    121,
+    111,
+    116,
+    111,
+    46,
+    109,
+    101,
+    100,
+    116,
+    101,
+    97,
+    51,
+    48,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+  var mt = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
 function secretaddress3() {
-	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,105,99,97,108,46,116,101,97,64,103,109,97,105,108,46,99,111,109);
-	var mt = String.fromCharCode(109,97,105,108,116,111,58);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+  var ma = String.fromCharCode(
+    107,
+    121,
+    111,
+    116,
+    111,
+    46,
+    109,
+    101,
+    100,
+    105,
+    99,
+    97,
+    108,
+    46,
+    116,
+    101,
+    97,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+  var mt = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
 
 //2021年度新歓用
 function secretaddress_2021() {
-	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,116,101,97,50,48,50,49,64,103,109,97,105,108,46,99,111,109);
-	var mt = String.fromCharCode(109,97,105,108,116,111,58);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+  var ma = String.fromCharCode(
+    107,
+    121,
+    111,
+    116,
+    111,
+    46,
+    109,
+    101,
+    100,
+    116,
+    101,
+    97,
+    50,
+    48,
+    50,
+    49,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+  var mt = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
 
 //2022年度新歓用
-function secretaddress_2022(){
-	var ma = String.fromCharCode(107,121,111,116,111,109,101,100,116,101,97,50,48,50,50,115,104,105,110,107,97,110,64,103,109,97,105,108,46,99,111,109);
-	var mt = String.fromCharCode(109,97,105,108,116,111,58);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');	
+function secretaddress_2022() {
+  var ma = String.fromCharCode(
+    107,
+    121,
+    111,
+    116,
+    111,
+    109,
+    101,
+    100,
+    116,
+    101,
+    97,
+    50,
+    48,
+    50,
+    50,
+    115,
+    104,
+    105,
+    110,
+    107,
+    97,
+    110,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+  var mt = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
 
 //2023年度新歓用
-function secretaddress_2023_twitter(){
-	var ma = String.fromCharCode(107,121,111,116,111,109,101,100,116,101,97,50,48,50,50,115,104,105,110,107,97,110,64,103,109,97,105,108,46,99,111,109);
-	var mt = String.fromCharCode(109,97,105,108,116,111,58);
-	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');	
+function secretaddress_2023_twitter() {
+  var ma = String.fromCharCode(
+    107,
+    121,
+    111,
+    116,
+    111,
+    109,
+    101,
+    100,
+    116,
+    101,
+    97,
+    50,
+    48,
+    50,
+    50,
+    115,
+    104,
+    105,
+    110,
+    107,
+    97,
+    110,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+  var mt = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+  document.write('<a href="' + mt + ma + '">' + ma + "</a>");
 }
 
+function secretaddress_gaimu() {}
